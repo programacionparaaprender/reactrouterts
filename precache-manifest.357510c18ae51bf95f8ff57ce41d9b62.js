fcc369b589c6c0cb1d3233c5bd9aed1ec163c892
Luis Correa <yancel209@gmail.com>
2021-02-14 17:57:32 -0500

    Updates

---
+++ precache-manifest.357510c18ae51bf95f8ff57ce41d9b62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f01f407a0686934269d07afb10d05a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30de2931e34a746d21bb",
+    "url": "/static/css/2.545c6387.chunk.css"
+  },
+  {
+    "revision": "3d9a22fd308283a3ca00",
+    "url": "/static/css/main.9614e64a.chunk.css"
+  },
+  {
+    "revision": "30de2931e34a746d21bb",
+    "url": "/static/js/2.e59595f9.chunk.js"
+  },
+  {
+    "revision": "3d9a22fd308283a3ca00",
+    "url": "/static/js/main.bd2c75ec.chunk.js"
+  },
+  {
+    "revision": "7885aeeb7225cebb7090",
+    "url": "/static/js/runtime-main.6fa0afa0.js"
+  }
+]);
